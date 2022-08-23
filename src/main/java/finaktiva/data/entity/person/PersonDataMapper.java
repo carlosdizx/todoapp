c62e2c6334ffaf9691ff7a17d433702e6269ec53
Carlos Diaz <carlos.diaz@ias.com.co>
2022-08-23 16:59:08 -0500

    Creado los objetos mapeadores entre objetos de dominio y base de datos

---
+++ src/main/java/finaktiva/data/entity/person/PersonDataMapper.java
@@ -0,0 +1,23 @@
+package finaktiva.data.entity.person;
+
+import finaktiva.domain.person.Person;
+import finaktiva.domain.person.PersonFactory;
+import finaktiva.mapper.IMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonDataMapper implements IMapper<Person, PersonData> {
+
+    @Override
+    public PersonData map(Person in) {
+        final PersonData personData = new PersonData();
+        BeanUtils.copyProperties(in, personData, "id");
+        return personData;
+    }
+
+    @Override
+    public Person iMap(PersonData out) {
+        return PersonFactory.createPerson(out);
+    }
+}
---
+++ src/main/java/finaktiva/domain/person/PersonFactory.java
@@ -0,0 +1,13 @@
+package finaktiva.domain.person;
+
+import finaktiva.data.entity.person.PersonData;
+
+public class PersonFactory {
+    public static Person createPerson(final PersonData personData) {
+        return Person.builder()
+                .id(personData.getId())
+                .names(personData.getNames())
+                .created_at(personData.getCreated_at())
+                .build();
+    }
+}
